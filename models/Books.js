e5139861e5f9d515724c0edaab64f34778008e48
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2022-10-24 22:31:24 -0600

    get request works in insomnia

--- models/Books.js
+++ models/Books.js
@@ -1,55 +1,55 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/conection');
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/conection');
 
-class Books extends Model {
+// class Books extends Model {
   
-}
+// }
 
-Books.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
+// Books.init(
+//   {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true,
+//     },
+//     title: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
       
-    },
-    author: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    synopsis: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    pagecount: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-    genre: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-          model: 'user',
-          key: 'id'
-      },
-    },
-  },
- {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'book',
-  },
-);
+//     },
+//     author: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     synopsis: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     pagecount: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false,
+//       },
+//     genre: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+    // user_id: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false,
+    //   references: {
+    //       model: 'user',
+    //       key: 'id'
+    //   },
+    // },
+//   },
+//  {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: 'book',
+//   },
+// );
 
-module.exports = Books;
+// module.exports = Books;
--- models/index.js
+++ models/index.js
@@ -1,30 +1,30 @@
 const User = require('./User');
-const Books = require('./Books');
-const Comments = require('./Comments');
+//const Books = require('./Books');
+// const Comments = require('./Comments');
 
-User.hasMany(Books, {
-    foreignKey: 'user_id'
-});
+// // User.hasMany(Books, {
+// //     foreignKey: 'user_id'
+// // });
 
-User.hasMany(Comments, {
-    foreignKey: 'user_id'
-});
+// User.hasMany(Comments, {
+//     foreignKey: 'user_id'
+// });
 
-Books.belongsTo(User, {
-    foreignKey: 'user_id'
-});
+// // Books.belongsTo(User, {
+// //     foreignKey: 'user_id'
+// // });
 
-Books.hasMany(Comments, {
-    foreignKey: 'user_id'
-});
+// // Books.hasMany(Comments, {
+// //     foreignKey: 'user_id'
+// // });
 
-Comments.belongsTo(User, {
-    foreignKey: 'user_id'
-});
+// Comments.belongsTo(User, {
+//     foreignKey: 'user_id'
+// });
 
-Comments.belongsTo(Books, {
-    foreignKey: 'user_id'
-});
+// Comments.belongsTo(Books, {
+//     foreignKey: 'user_id'
+// });
 
-module.exports = { User, Books, Comments };
+module.exports = { User, /*Books,Comments*/ };
 
